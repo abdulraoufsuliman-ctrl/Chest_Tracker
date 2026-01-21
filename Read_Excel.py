c72233acc62a9d0bbb827e686ec4f4790c40fdc9
abdulraoufsuliman-ctrl <abdulraoufsuliman@gmail.com>
2026-01-21 02:14:31 +0200

    Refactor Read_Excel.py to improve structure and style

--- Read_Excel.py
+++ Read_Excel.py
@@ -254,21 +254,22 @@ with tab1:
         f"<div class='tabs-date'>Last update: {get_file_modified_time('Results1.xlsx')}</div>",
         unsafe_allow_html=True
     )
-    load_and_display("Results1.xlsx")
+    load_and_display("Results1.xlsx", is_castle=False)
 
 with tab2:
     st.markdown(
         f"<div class='tabs-date'>Last update: {get_file_modified_time('Results2.xlsx')}</div>",
         unsafe_allow_html=True
     )
-    load_and_display("Results2.xlsx")
+    load_and_display("Results2.xlsx", is_castle=False)
 
 with tab3:
     st.markdown(
         f"<div class='tabs-date'>Last update: {get_file_modified_time('Results_Castle.xlsx')}</div>",
         unsafe_allow_html=True
     )
-    load_and_display("Results_Castle.xlsx")
+    load_and_display("Results_Castle.xlsx", is_castle=True)
+
 
 
 
