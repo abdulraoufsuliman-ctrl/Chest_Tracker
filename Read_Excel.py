8ccc464c552abaeb176b16874c7e446bfa8b95e8
abdulraoufsuliman-ctrl <abdulraoufsuliman@gmail.com>
2026-01-21 12:52:41 +0200

    Refactor Read_Excel.py to improve layout and styling

--- Read_Excel.py
+++ Read_Excel.py
@@ -231,7 +231,7 @@ def load_and_display(file_name, is_castle=False):
 
 # ================== Tabs (الفترات) ==================
 # تأكدنا هنا أن أسماء الفترات مكتوبة بوضوح
-tab1, tab2, tab3 = st.tabs(["Period 1", "Period 2", "Castle Competition"])
+tab1, tab2, tab3 = st.tabs(["Period 1", "Period 2",  "Period 3", "Castle Competition"])
 
 with tab1:
     st.markdown(
@@ -249,8 +249,17 @@ with tab2:
 
 with tab3:
     st.markdown(
+        f"<div class='tabs-date'>Last update: {get_file_modified_time('Results3.xlsx')}</div>",
+        unsafe_allow_html=True
+    )
+    load_and_display("Results3.xlsx", is_castle=False)
+
+
+with tab4:
+    st.markdown(
         f"<div class='tabs-date'>Last update: {get_file_modified_time('Results_Castle.xlsx')}</div>",
         unsafe_allow_html=True
     )
     load_and_display("Results_Castle.xlsx", is_castle=True)
+
 
