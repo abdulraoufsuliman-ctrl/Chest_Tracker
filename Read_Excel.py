35b606fb553ad80c32943237355be3b18ff0396d
abdulraoufsuliman-ctrl <243535915+abdulraoufsuliman-ctrl@users.noreply.github.com>
2026-01-23 17:17:18 +0200

    Refactor Read_Excel.py for improved styling and structure

--- Read_Excel.py
+++ Read_Excel.py
@@ -15,7 +15,14 @@ st.markdown("""
 .main {
     background-color: #f5f7fa;
 }
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 /* تقليل الفراغ العلوي وتوسيط المحتوى */
 .block-container {
     padding-top: 1.5rem;
@@ -263,6 +270,7 @@ with tab4:
         unsafe_allow_html=True
     )
     load_and_display("Results_Castle.xlsx", is_castle=True)
+
 
 
 
