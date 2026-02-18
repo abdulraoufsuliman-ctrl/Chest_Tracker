b5f4330081d57da22244df9e042cb5e81a0411f0
abdulraoufsuliman-ctrl <243535915+abdulraoufsuliman-ctrl@users.noreply.github.com>
2026-02-19 01:34:20 +0200

    Update Read_Excel.py

--- Read_Excel.py
+++ Read_Excel.py
@@ -256,7 +256,7 @@ def load_and_display(file_name, is_castle=False):
 # ================== Tabs (الفترات) ==================
 # تأكدنا هنا أن أسماء الفترات مكتوبة بوضوح
 #tab1, tab2, tab3, tab4 = st.tabs(["Period 1(18-24)", "Period 2(24-2/3)",  "Period 3(2-8)", "Last final results"])
-tab1, tab2, tab3, tab4 = st.tabs(["Period 1", "Period 2",  "Period 3", "final results"])
+tab1, tab2, tab3, tab4 = st.tabs(["Period 1", "Period 2",  "Period 3", "Castle Competition"])
 with tab1:
     st.markdown(
         f"<div class='tabs-date'>Last update: {get_file_modified_time('Results1.xlsx')}</div>",
@@ -285,6 +285,7 @@ with tab4:
         unsafe_allow_html=True
     )
     load_and_display("Results_Castle.xlsx", is_castle=True)
+
 
 
 
